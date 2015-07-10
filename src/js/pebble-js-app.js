d58a1c15e519a88e54ecd01392000e18c0ee6483
Andrew McOlash <amcolash@gmail.com>
2015-07-10 01:56:49 -0500

    Cache image in localstorage, date added

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,68 +1,85 @@
 var transferInProgress = false;
 
-Pebble.addEventListener("ready", function(e) {
+Pebble.addEventListener("ready", function (e) {
   console.log("NetDownload JS Ready");
 });
 
-Pebble.addEventListener("appmessage", function(e) {
+Pebble.addEventListener("appmessage", function (e) {
   console.log("Got message: " + JSON.stringify(e));
 
   if ('NETDL_URL' in e.payload) {
     if (transferInProgress === false) {
       transferInProgress = true;
-      downloadBinaryResource(e.payload.NETDL_URL, function(bytes) {
-        transferImageBytes(bytes, e.payload.NETDL_CHUNK_SIZE,
-          function() { console.log("Done!"); transferInProgress = false; },
-          function(e) { console.log("Failed! " + e); transferInProgress = false; }
-        );
-      },
-      function(e) {
-        console.log("Download failed: " + e); transferInProgress = false;
-      });
-    }
-    else {
+      downloadBinaryResource(e.payload.NETDL_URL, function (bytes) {
+          transferImageBytes(bytes, e.payload.NETDL_CHUNK_SIZE,
+            function () {
+              console.log("Done!");
+              transferInProgress = false;
+            },
+            function (e) {
+              console.log("Failed! " + e);
+              transferInProgress = false;
+            }
+          );
+        },
+        function (e) {
+          console.log("Download failed: " + e);
+          transferInProgress = false;
+        });
+    } else {
       console.log("Ignoring request to download " + e.payload.NETDL_URL + " because another download is in progress.");
     }
   }
 });
 
 function downloadBinaryResource(imageURL, callback, errorCallback) {
-    var req = new XMLHttpRequest();
-    req.open("GET", imageURL,true);
-    req.responseType = "arraybuffer";
-    req.onload = function(e) {
-        console.log("loaded");
-        var buf = req.response;
-        if(req.status == 200 && buf) {
-            var byteArray = new Uint8Array(buf);
-            var arr = [];
-            for(var i=0; i<byteArray.byteLength; i++) {
-                arr.push(byteArray[i]);
-            }
+  var currentDate = new Date();
+  var previousDate = new Date(parseInt(window.localStorage.getItem("previousDate")));
+  if (currentDate.getDate() === previousDate.getDate() && currentDate.getMonth() === previousDate.getMonth()) {
+    console.log("No need to download");
+    callback(JSON.parse(window.localStorage.getItem("image")));
+  } else {
 
-            console.log("Downloaded file with " + byteArray.length + " bytes.");
-            callback(arr);
+    var req = new XMLHttpRequest();
+    req.open("GET", imageURL, true);
+    req.responseType = "arraybuffer";
+    req.onload = function (e) {
+      console.log("loaded");
+      var buf = req.response;
+      if (req.status == 200 && buf) {
+        var byteArray = new Uint8Array(buf);
+        var arr = [];
+        for (var i = 0; i < byteArray.byteLength; i++) {
+          arr.push(byteArray[i]);
         }
-        else {
-          errorCallback("Request status is " + req.status);
-        }
+
+        window.localStorage.setItem("previousDate", currentDate.getTime());
+        window.localStorage.setItem("image", JSON.stringify(arr));
+        console.log(arr);
+
+        console.log("Downloaded file with " + byteArray.length + " bytes.");
+        callback(arr);
+      } else {
+        errorCallback("Request status is " + req.status);
+      }
     };
-    req.onerror = function(e) {
+    req.onerror = function (e) {
       errorCallback(e);
     };
     req.send(null);
+  }
 }
 
 function transferImageBytes(bytes, chunkSize, successCb, failureCb) {
   var retries = 0;
 
-  var success = function() {
+  var success = function () {
     console.log("Success cb=" + successCb);
     if (successCb !== undefined) {
       successCb();
     }
   };
-  var failure = function(e) {
+  var failure = function (e) {
     console.log("Failure cb=" + failureCb);
     if (failureCb !== undefined) {
       failureCb(e);
@@ -70,18 +87,22 @@ function transferImageBytes(bytes, chunkSize, successCb, failureCb) {
   };
 
   // This function sends chunks of data.
-  var sendChunk = function(start) {
+  var sendChunk = function (start) {
     var txbuf = bytes.slice(start, start + chunkSize);
     console.log("Sending " + txbuf.length + " bytes - starting at offset " + start);
-    Pebble.sendAppMessage({ "NETDL_DATA": txbuf },
-      function(e) {
+    Pebble.sendAppMessage({
+        "NETDL_DATA": txbuf
+      },
+      function (e) {
         // If there is more data to send - send it.
         if (bytes.length > start + chunkSize) {
           sendChunk(start + chunkSize);
         }
         // Otherwise we are done sending. Send closing message.
         else {
-          Pebble.sendAppMessage({"NETDL_END": "done" }, success, failure);
+          Pebble.sendAppMessage({
+            "NETDL_END": "done"
+          }, success, failure);
         }
       },
       // Failed to send message - Retry a few times.
@@ -89,8 +110,7 @@ function transferImageBytes(bytes, chunkSize, successCb, failureCb) {
         if (retries++ < 3) {
           console.log("Got a nack for chunk #" + start + " - Retry...");
           sendChunk(start);
-        }
-        else {
+        } else {
           failure(e);
         }
       }
@@ -98,7 +118,9 @@ function transferImageBytes(bytes, chunkSize, successCb, failureCb) {
   };
 
   // Let the pebble app know how much data we want to send.
-  Pebble.sendAppMessage({"NETDL_BEGIN": bytes.length },
+  Pebble.sendAppMessage({
+      "NETDL_BEGIN": bytes.length
+    },
     function (e) {
       // success - start sending
       sendChunk(0);
--- src/pebble-faces.c
+++ src/pebble-faces.c
@@ -7,9 +7,24 @@ static Window *window;
 static BitmapLayer *bitmap_layer;
 static GBitmap *current_bmp;
 
+static TextLayer *time_layer;
+static TextLayer *time_shadow0;
+static TextLayer *time_shadow1;
+static TextLayer *time_shadow2;
+static TextLayer *time_shadow3;
+
+static TextLayer *date_layer;
+static TextLayer *date_shadow0;
+static TextLayer *date_shadow1;
+static TextLayer *date_shadow2;
+static TextLayer *date_shadow3;
+
+static char buffer_time[] = "00:00";
+static char buffer_date[] = "00/00";
+
 static char *image = "http://pi.amcolash.com:5000/image.png";
 
-static int downloaded;
+struct tm *last_check;
 
 void show_image() {
   // show that we are loading by showing no image
@@ -26,16 +41,169 @@ void show_image() {
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
+  
   GRect bounds = layer_get_bounds(window_layer);
 
   bitmap_layer = bitmap_layer_create(bounds);
   layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer));
   current_bmp = NULL;
+  
+  int time_x = 0;
+  int time_y = 90;
+  // Set up main time layer
+  time_layer = text_layer_create(GRect(time_x, time_y, 144, 50));
+  text_layer_set_background_color(time_layer, GColorClear);
+  text_layer_set_text_color(time_layer, GColorWhite);
+  text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  
+  int time_thickness = 3;
+  // Set up shadow layers for time
+  // Left
+  time_shadow0 = text_layer_create(GRect(time_x - time_thickness, time_y, 144, 50));
+  text_layer_set_background_color(time_shadow0, GColorClear);
+  text_layer_set_font(time_shadow0, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(time_shadow0, GTextAlignmentCenter);
+  // Right
+  time_shadow1 = text_layer_create(GRect(time_x + time_thickness, time_y, 144, 50));
+  text_layer_set_background_color(time_shadow1, GColorClear);
+  text_layer_set_font(time_shadow1, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(time_shadow1, GTextAlignmentCenter);
+  // Up
+  time_shadow2 = text_layer_create(GRect(time_x, time_y - time_thickness, 144, 50));
+  text_layer_set_background_color(time_shadow2, GColorClear);
+  text_layer_set_font(time_shadow2, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(time_shadow2, GTextAlignmentCenter);
+  // Down
+  time_shadow3 = text_layer_create(GRect(time_x, time_y + time_thickness, 144, 50));
+  text_layer_set_background_color(time_shadow3, GColorClear);
+  text_layer_set_font(time_shadow3, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(time_shadow3, GTextAlignmentCenter);
+  
+  // Add time shadows to window
+  layer_add_child(window_layer, text_layer_get_layer(time_shadow0));
+  layer_add_child(window_layer, text_layer_get_layer(time_shadow1));
+  layer_add_child(window_layer, text_layer_get_layer(time_shadow2));
+  layer_add_child(window_layer, text_layer_get_layer(time_shadow3));
+  // Time after shadows
+  layer_add_child(window_layer, text_layer_get_layer(time_layer));
+  
+  int date_x = 0;
+  int date_y = 136;
+  // Set up main date layer
+  date_layer = text_layer_create(GRect(date_x, date_y, 144, 40));
+  text_layer_set_background_color(date_layer, GColorClear);
+  text_layer_set_text_color(date_layer, GColorWhite);
+  text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+  
+  int date_thickness = 2;
+  // Set up shadow layers for date
+  // Left
+  date_shadow0 = text_layer_create(GRect(date_x - date_thickness, date_y, 144, 40));
+  text_layer_set_background_color(date_shadow0, GColorClear);
+  text_layer_set_font(date_shadow0, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(date_shadow0, GTextAlignmentCenter);
+  // Right
+  date_shadow1 = text_layer_create(GRect(date_x + date_thickness, date_y, 144, 40));
+  text_layer_set_background_color(date_shadow1, GColorClear);
+  text_layer_set_font(date_shadow1, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(date_shadow1, GTextAlignmentCenter);
+  // Up
+  date_shadow2 = text_layer_create(GRect(date_x, date_y - date_thickness, 144, 40));
+  text_layer_set_background_color(date_shadow2, GColorClear);
+  text_layer_set_font(date_shadow2, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(date_shadow2, GTextAlignmentCenter);
+  // Down
+  date_shadow3 = text_layer_create(GRect(date_x, date_y + date_thickness, 144, 40));
+  text_layer_set_background_color(date_shadow3, GColorClear);
+  text_layer_set_font(date_shadow3, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(date_shadow3, GTextAlignmentCenter);
+  
+  // Add date shadows to window
+  layer_add_child(window_layer, text_layer_get_layer(date_shadow0));
+  layer_add_child(window_layer, text_layer_get_layer(date_shadow1));
+  layer_add_child(window_layer, text_layer_get_layer(date_shadow2));
+  layer_add_child(window_layer, text_layer_get_layer(date_shadow3));
+  // Date text after shadows
+  layer_add_child(window_layer, text_layer_get_layer(date_layer));
 }
 
-static void window_unload(Window *window) {
-  bitmap_layer_destroy(bitmap_layer);
-  gbitmap_destroy(current_bmp);
+static void update_time(struct tm *tick_time) {
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer_time, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12 hour format
+    int hour = tick_time->tm_hour;
+    strftime(buffer_time, sizeof("00:00"), "%l:%M", tick_time);
+    // Trim space character
+    if ((hour > 0 && hour < 12) || (hour > 12 && hour < 22)) {
+      int i;
+      for (i = 0; i < 4; i++) {
+        buffer_time[i] = buffer_time[i+1];
+      }
+      buffer_time[4] = '\0';
+    }
+  }
+  
+  int month = tick_time->tm_mon;
+  int date = tick_time->tm_mday;
+  int offset;
+  strftime(buffer_date, sizeof("00/00"), "%m/%d", tick_time);
+  // Trim 0 in month
+  if (month < 10) {
+    offset = 1;
+    int i;
+    for (i = 0; i < 4; i++) {
+      buffer_date[i] = buffer_date[i+1];
+    }
+    buffer_date[4] = '\0';
+  }
+  // Trim 0 in date
+  if (date < 10) {
+    int i;
+    for (i = 1+offset; i < 4+offset; i++) {
+      buffer_date[i] = buffer_date[i+1];
+    }
+    buffer_date[4+offset] = '\0';
+  }
+
+  // Display this time on the TextLayers
+  text_layer_set_text(time_layer, buffer_time);
+  text_layer_set_text(time_shadow0, buffer_time);
+  text_layer_set_text(time_shadow1, buffer_time);
+  text_layer_set_text(time_shadow2, buffer_time);
+  text_layer_set_text(time_shadow3, buffer_time);
+  
+  // Display the date on the TextLayers
+  text_layer_set_text(date_layer, buffer_date);
+  text_layer_set_text(date_shadow0, buffer_date);
+  text_layer_set_text(date_shadow1, buffer_date);
+  text_layer_set_text(date_shadow2, buffer_date);
+  text_layer_set_text(date_shadow3, buffer_date);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+//   // Use a long-lived buffer
+//   if (!current_bmp && lastCheck == 0) {
+//     printf("showimage\n");
+//     show_image();
+//     lastCheck = 15;
+//   } else if (!current_bmp) {
+//     printf("tick\n");
+//     lastCheck--;
+//   }
+  
+  if (!current_bmp || last_check->tm_mday != tick_time->tm_mday) {
+    printf("showimage\n");
+    show_image();
+    time_t temp = time(NULL);
+    last_check = localtime(&temp);
+  }
+  
+  update_time(tick_time);
 }
 
 void download_complete_handler(NetDownload *download) {
@@ -64,15 +232,15 @@ void download_complete_handler(NetDownload *download) {
   // We null it out now to avoid a double free
   download->data = NULL;
   netdownload_destroy(download);
+  
+  // Change tick handler to every minute now to save some battery
+  tick_timer_service_unsubscribe();
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  // Use a long-lived buffer
-  if (downloaded == 0) {
-    printf("Tick\n");
-    downloaded = 1;
-    show_image();
-  }
+static void window_unload(Window *window) {
+  bitmap_layer_destroy(bitmap_layer);
+  gbitmap_destroy(current_bmp);
 }
 
 static void init(void) {
